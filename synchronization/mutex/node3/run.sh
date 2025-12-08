097dd479b3bc3e84bfea6c7c382ab48e7d6b3148
Delai Resgista Setyawan <119137716+delairs@users.noreply.github.com>
2025-12-08 15:18:59 +0700

    dengan mutex

--- synchronization/mutex/node3/run.sh
+++ synchronization/mutex/node3/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 ./kv.py --id 3 --tcp 8003 --udp 8103 --peers 192.168.122.10:8001=1,192.168.122.11:8002=2 --logger-addr 192.168.122.13:9000 --numnodes 3 --use-mutex 0
+python3 ./kv.py --id 3 --tcp 8003 --udp 8103 --peers 192.168.122.10:8001=1,192.168.122.11:8002=2 --logger-addr 192.168.122.13:9000 --numnodes 3 --use-mutex 1
