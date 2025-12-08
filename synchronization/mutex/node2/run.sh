fa96d3430f88f22a9a3d48d223053213f40b29c8
Delai Resgista Setyawan <119137716+delairs@users.noreply.github.com>
2025-12-08 13:21:36 +0700

    tanpa mutex

--- synchronization/mutex/node2/run.sh
+++ synchronization/mutex/node2/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 ./kv.py --id 2 --tcp 8002 --udp 8102 --peers 192.168.122.10:8001=1,192.168.122.12:8003=3 --logger-addr 192.168.122.13:9000 --numnodes 3 --use-mutex 1
+python3 ./kv.py --id 2 --tcp 8002 --udp 8102 --peers 192.168.122.10:8001=1,192.168.122.12:8003=3 --logger-addr 192.168.122.13:9000 --numnodes 3 --use-mutex 0
--- synchronization/mutex/testing/test2.sh
+++ synchronization/mutex/testing/test2.sh
@@ -2,13 +2,13 @@
 #
 
 # Single PUT to node 1
-python3 ./kvclient.py --nodes 127.0.0.1:8001,127.0.0.1:8002,127.0.0.1:8003 cmd --node 1  "PUT color red"
-python3 ./kvclient.py --nodes 127.0.0.1:8001,127.0.0.1:8002,127.0.0.1:8003 cmd --node 2  "PUT color blue"
+python3 ./kvclient.py --nodes 192.168.122.10:8001,192.168.122.11:8002,192.168.122.12:8003 cmd --node 1  "PUT color red"
+python3 ./kvclient.py --nodes 192.168.122.10:8001,192.168.122.11:8002,192.168.122.12:8003 cmd --node 2  "PUT color blue"
 
 
 # GET from node 2
-python3 ./kvclient.py  --nodes 127.0.0.1:8001,127.0.0.1:8002,127.0.0.1:8003 cmd --node 2   "GET color"
-python3 ./kvclient.py  --nodes 127.0.0.1:8001,127.0.0.1:8002,127.0.0.1:8003 cmd --node 0   "GET color"
+python3 ./kvclient.py  --nodes 192.168.122.10:8001,192.168.122.11:8002,192.168.122.12:8003 cmd --node 2   "GET color"
+python3 ./kvclient.py  --nodes 192.168.122.10:8001,192.168.122.11:8002,192.168.122.12:8003 cmd --node 0   "GET color"
 
 
 
