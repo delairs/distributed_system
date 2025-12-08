715684609bc07bc8721c33eafd244a9596758127
Delai Resgista Setyawan <119137716+delairs@users.noreply.github.com>
2025-12-08 11:20:27 +0700

    adjust ip

--- synchronization/mutex/testing/test1.sh
+++ synchronization/mutex/testing/test1.sh
@@ -2,20 +2,16 @@
 #
 
 # Single PUT to node 1
-python3 ./kvclient.py --nodes 127.0.0.1:8001,127.0.0.1:8002,127.0.0.1:8003 cmd --node 1  "PUT color blue"
-
+python3 ./kvclient.py --nodes 192.168.122.10:8001,192.168.122.11:8002,192.168.122.12:8003 cmd --node 1  "PUT color blue"
 
 # GET from node 2
-python3 ./kvclient.py  --nodes 127.0.0.1:8001,127.0.0.1:8002,127.0.0.1:8003 cmd --node 2   "GET color"
-
+python3 ./kvclient.py  --nodes 192.168.122.10:8001,192.168.122.11:8002,192.168.122.12:8003 cmd --node 2   "GET color"
 
 # Race two writers (great for no-mutex demo)
-python3 ./kvclient.py --nodes 127.0.0.1:8001,127.0.0.1:8002,127.0.0.1:8003 race  "PUT color blue" "PUT color red"
-
+python3 ./kvclient.py --nodes 192.168.122.10:8001,192.168.122.11:8002,192.168.122.12:8003 race  "PUT color blue" "PUT color red"
 
 # Read the key from ALL nodes after the race
-python3 ./kvclient.py --nodes 127.0.0.1:8001,127.0.0.1:8002,127.0.0.1:8003  getall color
-
+python3 ./kvclient.py --nodes 192.168.122.10:8001,192.168.122.11:8002,192.168.122.12:8003  getall color
 
 
 
