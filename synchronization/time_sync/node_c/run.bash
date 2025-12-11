9552fa96632203072b3df1c5d7a3eee9846a129a
Delai Resgista Setyawan <119137716+delairs@users.noreply.github.com>
2025-12-11 15:26:23 +0700

    node_c

--- synchronization/time_sync/node_c/run.bash
+++ synchronization/time_sync/node_c/run.bash
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-python3 logger.py --bind 0.0.0.0 --port 9999 --expect 10
+python3 peer_node.py \
+	  --name C --listen 0.0.0.0 5002 \
+	  --peers A@198.162.122.10:5000 B@198.162.122.11:5001 C@198.162.122.12:5002 D@198.162.122.13 \
+	  --logger 198.162.122.14 9999 \
+	  --offset-ms -600
+
