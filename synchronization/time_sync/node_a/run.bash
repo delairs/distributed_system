271d635c71bfd4194f14ab2aac19fdbe9f3c697d
Delai Resgista Setyawan <119137716+delairs@users.noreply.github.com>
2025-12-11 22:05:06 +0700

    test

--- synchronization/time_sync/node_a/run.bash
+++ synchronization/time_sync/node_a/run.bash
@@ -4,6 +4,6 @@ python3 peer_node.py \
 	  --name A --listen 0.0.0.0 5000 \
 	  --peers A@192.168.122.10:5000 B@192.168.122.11:5001 C@192.168.122.12:5002 D@192.168.122.13:5003 \
 	  --logger 192.168.122.14 9999 \
-	  --offset-ms 600 \
+	  --offset-ms 8000 \
 	  --initiate-broadcast --msg "Hello from A"
 
